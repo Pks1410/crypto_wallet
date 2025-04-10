57e29f8781d5a5e5800035ac2695098279542afb
Pks1410 <1410sahu.prakhar@gmail.com>
2025-04-10 21:54:30 +0530

    effect changes

--- src/App.js
+++ src/App.js
@@ -1,77 +1,60 @@
-// import React from "react";
-// import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
-// import Navbar from "./components/Navbar";
-// // import CryptoMarket from "./components/cryptomarket";
-// import CryptoTable from "./components/cryptotable";
-// import WalletManager from "./components/walletManager";
-// import WalletDashboard from "./components/walletdashboard";
-// import Signup from "./signup";
-// import Login from "./login";
-// import "./App.css";
-
-// const App = () => {
-//   return (
-//     <Router>
-//       <Navbar /> {/* Navbar is always visible */}
-//       <div className="app-container">
-//         <Routes>
-//           {/* Default landing page */}
-//           <Route path="/" element={<CryptoTable />} />
-//           <Route path="/walletmanager" element={<WalletManager />} />
-//           <Route path="/walletdashboard" element={<WalletDashboard />} />
-//           <Route path="/signup" element={<Signup />} />
-//           <Route path="/login" element={<Login />} />
-//           <Route path="*" element={<Navigate to="/" />} /> {/* Redirect unknown routes */}
-//         </Routes>
-//       </div>
-//     </Router>
-//   );
-// };
-
-// export default App;
-
-import React from "react";
+import React, { useEffect } from "react";
+import { useLocation } from "react-router-dom";
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import CryptoTable from "./components/cryptotable";
-import CryptoConverter from "./components/cryptoconvertor"; // Importing CryptoConverter
+import CryptoConverter from "./components/cryptoconvertor";
 import WalletManager from "./components/walletManager";
 import WalletDashboard from "./components/walletdashboard";
 import Signup from "./signup";
 import Login from "./login";
 import "./App.css";
 import CryptoScroller from "./components/bottomnav";
+import ParticlesBackground from "./components/particles";
 
+const ScrollToTop = () => {
+  const { pathname } = useLocation();
+
+  React.useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+};
 const App = () => {
+
   return (
     <Router>
-      <Navbar /> {/* Navbar is always visible */}
-      <div className="app-container">
-        <Routes>
-          {/* Default landing page with Crypto Table & Converter */}
-          <Route 
-            path="/" 
-            element={
-              <div>
-                <CryptoTable />
-                <CryptoConverter />
-                <CryptoScroller/>
-              </div>
-            } 
-          />
-          <Route path="/walletmanager" element={<WalletManager />} />
-          <Route path="/walletdashboard" element={<WalletDashboard />} />
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="*" element={<Navigate to="/" />} /> {/* Redirect unknown routes */}
-        </Routes>
+      <div className="app-wrapper"> {/* New wrapper */}
+        <ParticlesBackground />
+        <ScrollToTop />
+      {/* App Content */}
+      <div className="app-content">
+        <Navbar />
+        <div className="app-container">
+          <Routes>
+            <Route 
+              path="/" 
+              element={
+                <>
+                  <CryptoTable />
+                  <CryptoConverter />
+                  <CryptoScroller />
+                </>
+              } 
+            />
+            <Route path="/walletManager" element={<WalletManager />} />
+            <Route path="/walletdashboard" element={<WalletDashboard />} />
+            <Route path="/signup" element={<Signup />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="*" element={<Navigate to="/" />} />
+          </Routes>
+        </div>
       </div>
+    </div>
     </Router>
   );
 };
 
 export default App;
-
-
-
 
